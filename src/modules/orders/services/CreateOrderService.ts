99a7922d0df85e387a7dedc400d8ff801c200b50
Jeison Azevedo <j3ison@gmail.com>
2020-05-20 01:12:58 -0300

    implementation done, creating migrations

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,33 @@ interface IRequest {
 @injectable()
 class CreateProductService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+    if (!customer) throw new AppError('Customer doesn`t exists.');
+
+    const findedProducts = await this.productsRepository.findAllById(products);
+
+    products.forEach(prod => {
+      const compareProd = findedProducts.find(p => p.id === prod.id);
+      if (!compareProd) throw new AppError('Products doesn`t exists');
+      if (prod.quantity > compareProd.quantity)
+        throw new AppError('Not enough products for this operation');
+    });
+
+    const orders = await this.ordersRepository.create({
+      customer,
+      products: findedProducts.map(p => ({ ...p, product_id: p.id })),
+    });
+
+    return orders;
   }
 }
 
---
+++ src/shared/infra/typeorm/migrations/1589943485949-CreateProduct.ts
@@ -0,0 +1,56 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateProduct1589943485949 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'price',
+            type: 'decimal(12,2)',
+          },
+          {
+            name: 'quantity',
+            type: 'int',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'OrderCustomer',
+            referencedTableName: 'customers',
+            columnNames: ['id'],
+            referencedColumnNames: [''],
+            onDelete: 'SET NULL',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('products');
+  }
+}
